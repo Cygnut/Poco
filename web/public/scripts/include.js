d4d666cc61920e21b00838cda43b9217e83711ff
Cygnut <lee.crosby@outlook.com>
2016-12-18 16:28:01 +0000

    =tidy up

--- web/public/scripts/include.js
+++ web/public/scripts/include.js
@@ -3,9 +3,7 @@ function include(file)
     document.write("<script type='text/javascript' src='" + file + "'></script>");
 }
 
-//include('http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js');
-include('https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js');
-
+include('vendor/angular.min-1.5.7.js');
 
 include('scripts/BackendClient.js');
 
